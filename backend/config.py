73b82076f628f359465553064b55c472707308a9
Muhammad Qasim <l215246@lhr.nu.edu.pk>
2025-01-04 19:54:55 +0500

    added configurations for the app

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+# Flask app configuration
+SECRET_KEY = '1234'
+
+# MongoDB configuration
+MONGO_URI = 'mongodb://localhost:27017/'
+MONGO_DBNAME = 'CyberMatrix'
+DB = ''
+# Flask-Mail configuration
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'mq08460@gmail.com'
+MAIL_PASSWORD = 'hrnu ohlj gcuo avly '
+
+MAIL_DEFAULT_SENDER = "mq08460@gmail.com" 
+
+BACKEND_URL = "http://localhost:5000"
+
