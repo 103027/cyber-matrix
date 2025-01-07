04f45b107f89c4684319be57e0cd9eee9c1c2d6f
Muhammad Qasim <l215246@lhr.nu.edu.pk>
2025-01-07 18:19:06 +0500

    initialize jwt in create app function

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,39 +1,10 @@
-# # app/__init__.py or wherever your create_app function is defined
-# from flask import Flask
-# from flask_mail import Mail
-# from pymongo import MongoClient
-
-# mail = Mail()
-
-# def create_app():
-#     global db
-#     app = Flask(__name__)
-
-#     # Load configuration
-#     app.config.from_pyfile('../config.py')
-
-#     # Initialize Flask-Mail
-#     mail.init_app(app)
-
-#     # Connect to MongoDB
-    
-#     client = MongoClient(app.config['MONGO_URI'])
-#     app.db = client[app.config['MONGO_DBNAME']] # Create db instance
-#     # app.config['DB'] = db  # Store db in the config object
-
-#     # Register routes after app is initialized
-#     from app.routes.auth import bp as auth_bp
-#     from app.routes.subdomains import bp as subdomains_bp
-#     app.register_blueprint(auth_bp)
-#     app.register_blueprint(subdomains_bp)
-    
-#     return app
-
 from flask import Flask
 from flask_mail import Mail
 from pymongo import MongoClient
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
+jwt = JWTManager()
 mail = Mail()
 
 def create_app():
@@ -55,6 +26,8 @@ def create_app():
     except Exception as e:
         raise RuntimeError(f"Failed to connect to MongoDB: {e}")
 
+    jwt.init_app(app)
+    
     # Register blueprints
     from app.routes.auth import bp as auth_bp
     from app.routes.subdomains import bp as subdomains_bp
